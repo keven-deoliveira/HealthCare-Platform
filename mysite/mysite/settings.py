e45c1858b260407490de709b295d59323f7858d5
keven-deoliveira <kevend@bu.edu>
2022-03-24 13:44:17 -0400

    cors stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "*"
 ]
 
 ROOT_URLCONF = 'mysite.urls'
